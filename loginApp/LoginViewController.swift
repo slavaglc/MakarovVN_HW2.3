6be0afa730a9c750107c655c265c2534ef4214b2
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-07-14 21:17:21 +0300

    Refactored  class LoginViewController added outlets

---
+++ loginApp/LoginViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var usernameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        
+        
+    }
+
+}
